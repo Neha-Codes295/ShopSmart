d5a39908c7d3e33499183870727eba764947d912
Neha-Codes295 <neha06012005@gmail.com>
2024-08-29 15:42:13 +0530

    Initial commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci ut non iure aliquam deserunt? Earum error illum libero 
+            it, amet consectetur adipisicing elit. Adipisci ut non iure aliquam deserunt? Earum error illum libero recusandae! Quae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, nobis.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
